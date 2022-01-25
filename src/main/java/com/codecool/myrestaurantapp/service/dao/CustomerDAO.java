8a927e6787f4569582a497facf22012d021eca9c
zsomborszego <szego.zsombor@gmail.com>
2022-01-25 17:16:16 +0100

    Feat: CustomerDaoMem can handle add,find,delete methods

--- src/main/java/com/codecool/myrestaurantapp/service/dao/CustomerDAO.java
+++ src/main/java/com/codecool/myrestaurantapp/service/dao/CustomerDAO.java
@@ -1,4 +1,12 @@
 package com.codecool.myrestaurantapp.service.dao;
 
+import com.codecool.myrestaurantapp.model.Customer;
+
+
 public interface CustomerDAO {
+    void addCustomer(Customer customer);
+    Customer findCustomer(String name);
+    void editCustomer(String name);
+    void deleteCustomer(String name);
+
 }
