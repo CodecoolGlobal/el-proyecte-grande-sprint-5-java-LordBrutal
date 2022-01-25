649b847923da8f89a3430eb1c289dd2027c433ee
zsomborszego <szego.zsombor@gmail.com>
2022-01-25 11:30:11 +0100

    add:  proxy for communication backend and frontend

--- src/main/java/com/codecool/myrestaurantapp/controller/AppiController.java
+++ src/main/java/com/codecool/myrestaurantapp/controller/AppiController.java
@@ -1,40 +1,43 @@
 package com.codecool.myrestaurantapp.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 public class AppiController {
 
+    @CrossOrigin
     @PostMapping(value = "/api/change-order-status")
     public String changeOrderStatus(){
         return "";
     }
 
+    @CrossOrigin
     @PostMapping(value = "/api/add-user")
     public String addNewUser(){
         return "";
     }
 
+    @CrossOrigin
     @PostMapping(value = "/api/add-order")
     public String addOrder(){
         return "";
     }
 
+    @CrossOrigin
     @PostMapping(value = "/api/add-reciept")
     public String addReciept(){
         return "";
     }
 
+    @CrossOrigin
     @PostMapping(value = "/api/add-ingredient")
     public String addIngredient(){
         return "";
     }
 
+    @CrossOrigin
     @GetMapping(value = "/api/get-orders")
     public  String getOrders(){
         return "";
