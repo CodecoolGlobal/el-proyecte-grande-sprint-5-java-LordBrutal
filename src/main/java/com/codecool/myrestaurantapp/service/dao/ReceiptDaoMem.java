dc4ac0ee5eba934e348b12bfc007cb41ef9f0a34
LordBrutal <bogar.jozsef@gmail.com>
2022-01-25 18:03:26 +0100

    add: implement methods

--- src/main/java/com/codecool/myrestaurantapp/service/dao/ReceiptDaoMem.java
+++ src/main/java/com/codecool/myrestaurantapp/service/dao/ReceiptDaoMem.java
@@ -1,7 +1,38 @@
 package com.codecool.myrestaurantapp.service.dao;
 
+import com.codecool.myrestaurantapp.model.Receipt;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Repository
 public class ReceiptDaoMem implements ReceiptDAO{
+
+    private Set<Receipt> receipts = new HashSet<>();
+
+    @Override
+    public void addNewReceipt(Receipt receipt) {
+        receipts.add(receipt);
+    }
+
+    @Override
+    public Receipt modifyReceipt(int receiptId) {
+        for (Receipt receipt : receipts) {
+            if (receiptId==receipt.getId()) {
+                return receipt;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteReceipt(int receiptId) {
+        receipts.removeIf(receipt -> receiptId == receipt.getId());
+    }
+
+    @Override
+    public Set<Receipt> getAllReceipt() {
+        return receipts;
+    }
 }
