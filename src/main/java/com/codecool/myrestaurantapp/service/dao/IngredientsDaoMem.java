7176e5b02afaa0c83b0d8459d7e4a960d6ac4e81
LordBrutal <bogar.jozsef@gmail.com>
2022-01-25 16:24:25 +0100

    add: four DaoMem

---
+++ src/main/java/com/codecool/myrestaurantapp/service/dao/IngredientsDaoMem.java
@@ -0,0 +1,7 @@
+package com.codecool.myrestaurantapp.service.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class IngredientsDaoMem implements IngredientsDAO{
+}
---
+++ src/main/java/com/codecool/myrestaurantapp/service/dao/OrderDaoMem.java
@@ -0,0 +1,7 @@
+package com.codecool.myrestaurantapp.service.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OrderDaoMem implements OrderDAO{
+}
