c3bea31a601ff1fd0e2077646df0264262b5fa82
zsomborszego <szego.zsombor@gmail.com>
2022-01-25 10:53:00 +0100

    add: AppiController.java to handle endpoints

---
+++ src/main/java/com/codecool/myrestaurantapp/controller/AppiController.java
@@ -0,0 +1,12 @@
+package com.codecool.myrestaurantapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class AppiController {
+
+
+}
