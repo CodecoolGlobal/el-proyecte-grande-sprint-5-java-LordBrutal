357985a33aadb473a73b9c2d1141c1ce364f1ac5
LordBrutal <bogar.jozsef@gmail.com>
2022-01-27 00:02:34 +0100

    refactor: remove unused imports

--- src/main/java/com/codecool/myrestaurantapp/service/dao/OrderDAO.java
+++ src/main/java/com/codecool/myrestaurantapp/service/dao/OrderDAO.java
@@ -1,10 +1,6 @@
 package com.codecool.myrestaurantapp.service.dao;
 
-import com.codecool.myrestaurantapp.model.Customer;
 import com.codecool.myrestaurantapp.model.Order;
-import com.codecool.myrestaurantapp.model.Receipt;
-
-import java.util.List;
 import java.util.Set;
 
 public interface OrderDAO {
