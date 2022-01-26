846dab634859a208ec713668d27479298421d852
zsomborszego <szego.zsombor@gmail.com>
2022-01-26 16:25:20 +0100

    fix: remove code, add route managger for enpoints

--- src/frontend/myrestaurantapp/src/App.js
+++ src/frontend/myrestaurantapp/src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
+import React from "react";
+import Main from "./pages/Main";
+import {BrowserRouter as Router, Switch, Route, Routes} from "react-router-dom";
+import "./App.css"
 import Navbar from "./Navbar";
+import Ingredients from "./pages/Ingredients";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Navbar/>
+                <Routes>
+                    <Route path="/" element={<Main/>} exact/>
+                    <Route path="/ingredient" element={<Ingredients/>}/>
+                </Routes>
+
+            </div>
+        </Router>
+    )
 }
 
 export default App;
