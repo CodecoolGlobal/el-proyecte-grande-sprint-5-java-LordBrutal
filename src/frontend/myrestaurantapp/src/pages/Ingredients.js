3c4a002876e333984ad6ce360be7af6a7956a829
zsomborszego <szego.zsombor@gmail.com>
2022-01-26 18:11:45 +0100

    add: fields for user inpot

--- src/frontend/myrestaurantapp/src/pages/Ingredients.js
+++ src/frontend/myrestaurantapp/src/pages/Ingredients.js
@@ -1,10 +1,24 @@
-
 function Ingredients() {
-    return (<div>
-        <form>
-            <label>Name<input type="text" name="name" /></label>
-            <button type="submit">Click me</button>
-        </form>
+    return (
+        <div className="add-ingredient-container">
+            <div className="add-ingredient-baackround">
+                <form action="http://localhost:8080/api/add-ingredient" method="POST">
+                    <label className="label-text">Ingredient name<br/>
+                        <input  className="label-input" type="text" name="name" required/>
+                    </label><br/>
+                    <label className="label-text">Price<br/>
+                        <input className="label-input" type="text" name="price" required/>
+                    </label><br/>
+                    <label className="label-text">Type<br/>
+                        <select className="select-dropdown" name="price">
+                            <option value="GRAM">gram</option>
+                            <option value="MILLILITRE">milliliter</option>
+                            <option value="PIECE">piece</option>
+                        </select>
+                    </label><br/>
+                    <button className="submit-button" type="submit">Save me</button>
+                </form>
+            </div>
     </div>)
 }
 
