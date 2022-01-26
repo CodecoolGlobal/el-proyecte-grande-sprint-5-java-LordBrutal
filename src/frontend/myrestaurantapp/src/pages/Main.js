5432f140b90f8b7c76c743c43b60e638689b14ed
zsomborszego <szego.zsombor@gmail.com>
2022-01-26 18:11:30 +0100

    add: chef image

--- src/frontend/myrestaurantapp/src/pages/Main.js
+++ src/frontend/myrestaurantapp/src/pages/Main.js
@@ -1,7 +1,12 @@
+import chef from "../images/logoChef.svg"
+
 function Main(){
     return (
         <div>
             <h1>Hello please choose some on the function </h1>
+            <div className="center-img">
+                <img className="chef-logo" src={chef} alt=""/>
+            </div>
         </div>
     )
 }
