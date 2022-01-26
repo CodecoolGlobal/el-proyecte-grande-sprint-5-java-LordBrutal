7f7d43f90512b7281c31221148135643d194b314
zsomborszego <szego.zsombor@gmail.com>
2022-01-26 18:12:39 +0100

    refactor: delet empty row

--- src/frontend/myrestaurantapp/src/App.js
+++ src/frontend/myrestaurantapp/src/App.js
@@ -14,7 +14,6 @@ function App() {
                     <Route path="/" element={<Main/>} exact/>
                     <Route path="/ingredient" element={<Ingredients/>}/>
                 </Routes>
-
             </div>
         </Router>
     )
