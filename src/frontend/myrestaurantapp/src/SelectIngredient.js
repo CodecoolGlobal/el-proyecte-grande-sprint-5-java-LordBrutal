711fec339eeb573338e65e68f0e84546090f009d
zsomborszego <szego.zsombor@gmail.com>
2022-01-27 15:06:36 +0100

    add: JSON api call for datas

--- src/frontend/myrestaurantapp/src/SelectIngredient.js
+++ src/frontend/myrestaurantapp/src/SelectIngredient.js
@@ -1,43 +1,35 @@
-function  SelectIngredient(){
-    const json = [
-        {
-            "name": "pepper",
-            "price": 200,
-            "quantity": "GRAM"
-        },
-        {
-            "name": "egg",
-            "price": 1400,
-            "quantity": "PIECE"
-        },
-        {
-            "name": "red wine",
-            "price": 5000,
-            "quantity": "MILLILITRE"
-        },
-        {
-            "name": "salt",
-            "price": 200,
-            "quantity": "GRAM"
-        },
-        {
-            "name": "flour",
-            "price": 310,
-            "quantity": "GRAM"
-        },
-        {
-            "name": "beef",
-            "price": 8000,
-            "quantity": "GRAM"
-        }
-    ]
+import {useEffect, useState} from "react";
 
+function SelectIngredient() {
+    const [jsn, setjsn] = useState({isLoaded: false});
+    useEffect(() => {
+        fetch("http://localhost:8080/api/get-all-ingredient")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setjsn({data : result, isLoaded: true})
+                },
+                (error) => {
+                    setjsn({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    },[]);
+
+    if (jsn.isLoaded && jsn.error === undefined){
+        return (<div><select className="select-dropdown" name="ingredient" id="slects">
+            {jsn.data.map((row, index) => (
+                <option key={index} value={row.name}>{row.name}</option>
+            ))}
+        </select>
+            <label><input className="piece-input" type="number" name="quantity"/></label></div>)
+    }
     return (<div><select className="select-dropdown" name="ingredient" id="slects">
-        {json.map((row,index) => (
-            <option key={index} value={row.name}>{row.name}</option>
-        ))}
+            <option  value="">""</option>
     </select>
-        <label><input className="piece-input" type="number" name="quantity" /></label></div>)
+        <label><input className="piece-input" type="number" name="quantity"/></label></div>)
 }
 
 export default SelectIngredient;
