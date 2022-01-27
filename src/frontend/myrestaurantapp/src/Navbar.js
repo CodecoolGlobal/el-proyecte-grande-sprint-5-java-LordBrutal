31e7eee758923b9445c615fc20b1cdd63117d22d
LordBrutal <bogar.jozsef@gmail.com>
2022-01-27 18:53:38 +0100

    add: 2 new NavbarListItem

--- src/frontend/myrestaurantapp/src/Navbar.js
+++ src/frontend/myrestaurantapp/src/Navbar.js
@@ -1,3 +1,6 @@
+import NavbarListItem from "./components/NavbarListItem";
+
+
 function Navbar() {
     return (
         <div>
@@ -7,6 +10,9 @@ function Navbar() {
                     <li><a href="/ingredient">Add ingredient</a></li>
                     <li><a href="/create-recipe">Create recipe</a></li>
                     <li><a href="/create-customer">Add Customer</a></li>
+                    <NavbarListItem url={"/create-order"} text={"Add new order"}/>
+                    <NavbarListItem url={"/list-active-orders"} text={"Active orders"}/>
+                    <li><a href="#">About</a></li>
                 </ul>
             </div>
         </div>
