2626f30e7692400e218ad367a6a2ac8da74ad0f8
LordBrutal <bogar.jozsef@gmail.com>
2022-01-27 09:48:04 +0100

    modify: revert to initial state

--- src/main/java/com/codecool/myrestaurantapp/controller/ApiController.java
+++ src/main/java/com/codecool/myrestaurantapp/controller/ApiController.java
@@ -1,27 +1,10 @@
 package com.codecool.myrestaurantapp.controller;
 
-import com.codecool.myrestaurantapp.model.Order;
-import com.codecool.myrestaurantapp.model.Receipt;
-import com.codecool.myrestaurantapp.service.OrderService;
-import com.codecool.myrestaurantapp.service.ReceiptService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.*;
 
 @RestController
 @RequestMapping("/")
 public class ApiController {
-
-    OrderService orderService;
-    ReceiptService receiptService;
-
-    @Autowired
-    public ApiController(OrderService orderService, ReceiptService receiptService) {
-        this.orderService = orderService;
-        this.receiptService = receiptService;
-    }
 
     @CrossOrigin
     @PostMapping(value = "/api/change-order-status")
@@ -37,27 +20,20 @@ public class ApiController {
 
     @CrossOrigin
     @PostMapping(value = "/api/add-order")
-    public Order addOrder(HttpServletRequest request){
-        Map<String, String[]> parameterMap = request.getParameterMap();
-        String customerName = parameterMap.get("customer")[0];
-        String[] foods = parameterMap.get("foods");
-        return orderService.addNewOrder(foods,customerName);
+    public String addOrder(){
+        return "";
     }
 
     @CrossOrigin
     @GetMapping(value = "/api/get-all-receipt")
-    public Set<Receipt> getAllReceipt() {
-        return receiptService.getAllReceipt();
+    public String getAllReceipt() {
+        return "";
     }
 
     @CrossOrigin
     @PostMapping(value = "/api/add-receipt")
-    public Receipt addReceipt(HttpServletRequest request){
-        Map<String, String[]> parameterMap = request.getParameterMap();
-        String name = parameterMap.get("name")[0];
-        String[] ingredients = parameterMap.get("ingredients");
-        int price = Integer.parseInt(parameterMap.get("price")[0]);
-        return receiptService.addNewReceipt(name, ingredients, price);
+    public String addReceipt(){
+        return "";
     }
 
     @CrossOrigin
@@ -68,8 +44,8 @@ public class ApiController {
 
     @CrossOrigin
     @GetMapping(value = "/api/get-orders")
-    public Set<Order> getActiveOrders(){
-        return orderService.getActiveOrders();
+    public String getActiveOrders(){
+        return "";
     }
 
     @CrossOrigin
