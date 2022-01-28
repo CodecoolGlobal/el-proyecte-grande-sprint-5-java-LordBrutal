283415197ca529f9596b381d7c822f0a908e9dee
LordBrutal <bogar.jozsef@gmail.com>
2022-01-28 01:03:50 +0100

    add: method to format raw LocalDateTime

--- src/main/java/com/codecool/myrestaurantapp/model/Order.java
+++ src/main/java/com/codecool/myrestaurantapp/model/Order.java
@@ -6,6 +6,7 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Data
@@ -18,11 +19,16 @@ public class Order {
     private Customer customer;
     private LocalDateTime orderTime;
     private BigDecimal totalPrice;
+    private String formattedDateTime;
 
     public void countTotalPrice() {
         totalPrice = BigDecimal.ZERO;
         for (Receipt food : foods) {
             totalPrice = totalPrice.add(food.getPrice());
         }
+    }
+    public void formatDate() {
+    DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    formattedDateTime = orderTime.format(format);
     }
 }
