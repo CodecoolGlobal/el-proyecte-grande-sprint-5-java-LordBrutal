287097817fba47a1b931896ad8ea719f690e1c0d
zsomborszego <szego.zsombor@gmail.com>
2022-01-28 08:45:19 +0100

    fix: turn off inpunt field auto complete

--- src/frontend/myrestaurantapp/src/pages/AddCustomer.js
+++ src/frontend/myrestaurantapp/src/pages/AddCustomer.js
@@ -3,22 +3,22 @@ function AddCustomer() {
         <div className="add-user-baackround">
             <form action="http://localhost:8080/api/add-customer" method="POST">
                 <label className="label-text">Name<br/>
-                    <input className="label-input" type="text"  width="100px" name="name" required/>
+                    <input className="label-input" type="text"  width="100px" name="name" autoComplete="off" required/>
                 </label><br/>
                 <label className="label-text">Email<br/>
-                    <input className="label-input" type="text" name="email" required/>
+                    <input className="label-input" type="text" name="email" autoComplete="off" required/>
                 </label><br/>
                 <label className="label-text">Phone Number<br/>
-                    <input className="label-input" type="text" name="phoneNumber" required/>
+                    <input className="label-input" type="text" name="phoneNumber" autoComplete="off" required/>
                 </label><br/>
                 <label className="label-text">City Name<br/>
-                    <input className="label-input" type="text" name="cityName" required/>
+                    <input className="label-input" type="text" name="cityName" autoComplete="off" required/>
                 </label><br/>
                 <label className="label-text">Street Name<br/>
-                    <input className="label-input" type="text" name="streetName" required/>
+                    <input className="label-input" type="text" name="streetName" autoComplete="off" required/>
                 </label><br/>
                 <label className="label-text">House Number<br/>
-                    <input className="label-input" type="number" name="houseNumber" required/>
+                    <input className="label-input" type="number" name="houseNumber" autoComplete="off" required/>
                 </label><br/>
                 <button className="submit-button" type="submit">Save me</button>
             </form>
