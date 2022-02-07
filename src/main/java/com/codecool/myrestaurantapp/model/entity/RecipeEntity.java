3c04c3b4b9e601a2c969bdd885c1ff33db70e964
zsomborszego <szego.zsombor@gmail.com>
2022-02-07 16:15:42 +0100

    add: RecipeEntity

---
+++ src/main/java/com/codecool/myrestaurantapp/model/entity/RecipeEntity.java
@@ -0,0 +1,41 @@
+package com.codecool.myrestaurantapp.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "recipe")
+public class RecipeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @ManyToMany(mappedBy = "foods")
+    private List<OrderEntity> orders;
+
+    private String name;
+
+    @ManyToMany
+    private List<IngredientEntity> ingredients;
+    private BigDecimal price;
+    private boolean isAvailable;
+
+    public RecipeEntity(List<OrderEntity> orders, String name, List<IngredientEntity> ingredients, BigDecimal price, boolean isAvailable) {
+        this.orders = orders;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.price = price;
+        this.isAvailable = isAvailable;
+    }
+
+    public RecipeEntity() {
+    }
+}
