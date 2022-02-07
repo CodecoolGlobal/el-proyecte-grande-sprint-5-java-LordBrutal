ba2ca0abf2f3799c5bc1bbc5ce7ce2abab79a60c
LordBrutal <bogar.jozsef@gmail.com>
2022-02-07 15:01:01 +0100

    add: AddressEntity

---
+++ src/main/java/com/codecool/myrestaurantapp/model/entity/AddressEntity.java
@@ -0,0 +1,32 @@
+package com.codecool.myrestaurantapp.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "address")
+public class AddressEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String cityName;
+    private String streetName;
+    private int houseNumber;
+    @OneToMany(mappedBy = "address")
+    private Set<CustomerEntity> customers;
+
+    public AddressEntity(String cityName, String streetName, int houseNumber) {
+        this.cityName = cityName;
+        this.streetName = streetName;
+        this.houseNumber = houseNumber;
+    }
+
+    public AddressEntity() {
+    }
+}
