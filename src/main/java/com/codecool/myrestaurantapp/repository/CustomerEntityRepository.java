0b386e558997b2a237b0025c763b6e9131dc93a7
LordBrutal <bogar.jozsef@gmail.com>
2022-02-08 15:53:26 +0100

    add: new method to CustomerEntityRepository

--- src/main/java/com/codecool/myrestaurantapp/repository/CustomerEntityRepository.java
+++ src/main/java/com/codecool/myrestaurantapp/repository/CustomerEntityRepository.java
@@ -4,4 +4,6 @@ import com.codecool.myrestaurantapp.model.entity.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerEntityRepository extends JpaRepository<CustomerEntity, Long> {
+
+    CustomerEntity findCustomerEntityById(Long id);
 }
