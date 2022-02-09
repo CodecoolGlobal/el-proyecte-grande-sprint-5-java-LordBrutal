c475ab1ba08f505bd1c20eb85cdf62d28f60d3ce
LordBrutal <bogar.jozsef@gmail.com>
2022-02-09 16:30:04 +0100

    add: constructor with RecipeEntity type parameter

--- src/main/java/com/codecool/myrestaurantapp/model/Receipt.java
+++ src/main/java/com/codecool/myrestaurantapp/model/Receipt.java
@@ -1,5 +1,6 @@
 package com.codecool.myrestaurantapp.model;
 
+import com.codecool.myrestaurantapp.model.entity.RecipeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,4 +18,15 @@ public class Receipt {
     private List<RecipeIngredient> ingredientList;
     private BigDecimal price;
     private boolean isAvailable;
+
+    public Receipt(RecipeEntity recipeEntity) {
+        this.id = recipeEntity.getId();
+        this.name = recipeEntity.getName();
+        this.ingredientList = recipeEntity.getIngredientEntityList()
+                .stream()
+                .map(RecipeIngredient::new)
+                .toList();
+        this.price = recipeEntity.getPrice();
+        this.isAvailable = recipeEntity.isAvailable();
+    }
 }
