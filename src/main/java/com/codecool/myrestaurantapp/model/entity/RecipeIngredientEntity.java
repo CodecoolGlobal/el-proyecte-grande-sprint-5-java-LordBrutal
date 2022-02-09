841db03d67e2966b7438a5b5e57535dcf7e8d34e
LordBrutal <bogar.jozsef@gmail.com>
2022-02-09 16:31:09 +0100

    add: RecipeIngredientEntity

---
+++ src/main/java/com/codecool/myrestaurantapp/model/entity/RecipeIngredientEntity.java
@@ -0,0 +1,26 @@
+package com.codecool.myrestaurantapp.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "recipe_ingredient")
+public class RecipeIngredientEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "ingredient_id")
+    private IngredientEntity ingredientEntity;
+    @ManyToOne
+    @JoinColumn(name = "recipe_id")
+    private RecipeEntity recipeEntity;
+    private Long quantity;
+
+}
