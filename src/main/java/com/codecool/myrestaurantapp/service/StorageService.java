a9c0b2223af1b6fb895f1253b65951d555022863
zsomborszego <szego.zsombor@gmail.com>
2022-02-09 16:26:03 +0100

    feat: user can add an Ingredient to the storage

--- src/main/java/com/codecool/myrestaurantapp/service/StorageService.java
+++ src/main/java/com/codecool/myrestaurantapp/service/StorageService.java
@@ -1,12 +1,39 @@
 package com.codecool.myrestaurantapp.service;
 
 import com.codecool.myrestaurantapp.model.entity.IngredientEntity;
-import com.codecool.myrestaurantapp.model.type.UnitOfMeasureType;
+import com.codecool.myrestaurantapp.model.entity.StorageEntity;
+import com.codecool.myrestaurantapp.repository.IngredientEntityRepository;
+import com.codecool.myrestaurantapp.repository.StorageEntityRepositpry;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.Map;
+
+
 @Service
 public class StorageService {
 
+
+    StorageEntityRepositpry storageEntityRepositpry;
+    IngredientEntityRepository ingredientEntityRepository;
+
+    public StorageService(StorageEntityRepositpry storageEntityRepositpry, IngredientEntityRepository ingredientEntityRepository) {
+        this.storageEntityRepositpry = storageEntityRepositpry;
+        this.ingredientEntityRepository = ingredientEntityRepository;
+    }
+
+    public void addIngredient(Map<String, String[]> parameters){
+        String[] ingredientName = parameters.get("id");
+        String[] quantity = parameters.get("quantity");
+        if(storageEntityRepositpry.findByIngredientId(Long.parseLong(ingredientName[0])).isPresent()){
+            System.out.println("van benne");
+            StorageEntity storageEntity = storageEntityRepositpry.findByIngredientId(Long.parseLong(ingredientName[0])).get();
+            storageEntity.addQuantity(Long.parseLong(quantity[0]));
+            storageEntityRepositpry.save(storageEntity);
+        }else {
+            IngredientEntity ingredientEntity = ingredientEntityRepository.findIngredientEntityById(Long.parseLong(ingredientName[0]));
+            StorageEntity storageEntity = new StorageEntity(ingredientEntity,Long.parseLong(quantity[0]));
+            storageEntityRepositpry.save(storageEntity);
+        }
+
+    }
 }
