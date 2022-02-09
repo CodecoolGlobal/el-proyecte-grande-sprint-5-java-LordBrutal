9ba919f6800e67ead639f234c25fff97e8197b48
zsomborszego <szego.zsombor@gmail.com>
2022-02-09 13:58:10 +0100

    add: StorageSevice

---
+++ src/main/java/com/codecool/myrestaurantapp/service/StorageService.java
@@ -0,0 +1,12 @@
+package com.codecool.myrestaurantapp.service;
+
+import com.codecool.myrestaurantapp.model.entity.IngredientEntity;
+import com.codecool.myrestaurantapp.model.type.UnitOfMeasureType;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Map;
+@Service
+public class StorageService {
+
+}
