a5fe7b15bb28a2cda3381dab4f71deb7b5c6e953
zsomborszego <szego.zsombor@gmail.com>
2022-02-23 14:40:24 +0100

    fix: error page by permission

--- src/frontend/myrestaurantapp/src/Navbar.js
+++ src/frontend/myrestaurantapp/src/Navbar.js
@@ -8,27 +8,31 @@ function Navbar() {
     const location = useLocation();
     let errorPage = false;
     const routes = ["/logout","/ingredient", "/create-recipe", "/create-customer", "/create-order", "/list-active-orders", "/login", "/"]
+    const defaultRutes = ["/login", "/"]
     if (routes.indexOf(useLocation().pathname) === -1){
         errorPage = true;
     }
 
-    if (!errorPage && validateRoleUser()){
-        return (
-            <div>
-                <div className="header-navbar">
-                    <ul>
-                        {validateRoleUser() ? (<div><li><a href="#">Logout</a></li>
-                            <li><a href="/ingredient">Add ingredient</a></li>
-                            <li><a href="/create-recipe">Create recipe</a></li>
-                            <li><a href="/create-customer">Add Customer</a></li>
-                            <NavbarListItem url={"/create-order"} text={"Add new order"}/>
-                            <NavbarListItem url={"/list-active-orders"} text={"Active orders"}/>
-                            <li><a href="#">About</a></li></div>) : location.pathname === "/login" ? null :(<li><a href="/login">Login</a></li> )}
-                        <li id="logo-left" className="animation-top"><img className="main-logo" src={logo} alt="logo"/></li>
-                        <li id="left-logo-text"  className="animation-top"><a href="/">My Restaurant</a></li>
-                    </ul>
-                </div>
-            </div>)
+    if (!errorPage){
+        if(!validateRoleUser() && defaultRutes.indexOf(location.pathname) === -1){
+            return (<div></div>)
+        }else {
+            return (
+                <div>
+                    <div className="header-navbar">
+                        <ul>
+                            {validateRoleUser() ? (<div><li><a href="#">Logout</a></li>
+                                <li><a href="/ingredient">Add ingredient</a></li>
+                                <li><a href="/create-recipe">Create recipe</a></li>
+                                <li><a href="/create-customer">Add Customer</a></li>
+                                <NavbarListItem url={"/create-order"} text={"Add new order"}/>
+                                <NavbarListItem url={"/list-active-orders"} text={"Active orders"}/>
+                                <li><a href="#">About</a></li></div>) : location.pathname === "/login" ? null :(<li><a href="/login">Login</a></li> )}
+                            <li id="logo-left" className="animation-top"><img className="main-logo" src={logo} alt="logo"/></li>
+                            <li id="left-logo-text"  className="animation-top"><a href="/">My Restaurant</a></li>
+                        </ul>
+                    </div>
+                </div>)}
     }else return (<div></div>)
 }
 
