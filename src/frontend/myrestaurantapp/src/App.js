f71ffc7b0483eee2f7521e26f41201194b60e973
zsomborszego <szego.zsombor@gmail.com>
2022-02-23 11:58:17 +0100

    add: permission based endpoints

--- src/frontend/myrestaurantapp/src/App.js
+++ src/frontend/myrestaurantapp/src/App.js
@@ -8,6 +8,9 @@ import CreateRecipe from "./pages/CreateRecipe";
 import AddCustomer from "./pages/AddCustomer";
 import AddNewOrder from "./pages/AddNewOrder";
 import ActiveOrders from "./pages/ActiveOrders";
+import Login from "./pages/Login";
+import {getUserToken} from "./services/AuthService";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
     return (
@@ -16,11 +19,13 @@ function App() {
                 <Navbar/>
                 <Routes>
                     <Route path="/" element={<Main/>} exact/>
-                    <Route path="/ingredient" element={<Ingredients/>}/>
-                    <Route path="/create-recipe" element={<CreateRecipe/>}/>
-                    <Route path="/create-customer" element={<AddCustomer/>}/>
-                    <Route path={"/create-order"} element={<AddNewOrder/>}/>
-                    <Route path="/list-active-orders" element={<ActiveOrders/>}/>
+                    {getUserToken() && <Route path="/ingredient" element={<Ingredients/>}/> &&
+                    <Route path="/create-recipe" element={<CreateRecipe/>}/> &&
+                    <Route path="/create-customer" element={<AddCustomer/>}/> &&
+                    <Route path={"/create-order"} element={<AddNewOrder/>}/> &&
+                    <Route path="/list-active-orders" element={<ActiveOrders/>}/>}
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="*" element={<PageNotFound/>}/>
                 </Routes>
             </div>
         </Router>
