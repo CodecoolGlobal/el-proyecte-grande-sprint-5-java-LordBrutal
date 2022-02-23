28fe1f488b1c853ad327d4126bcb78a003258cc2
LordBrutal <bogar.jozsef@gmail.com>
2022-02-23 10:02:26 +0100

    add: UserEntity class

---
+++ src/main/java/com/codecool/myrestaurantapp/model/entity/UserEntity.java
@@ -0,0 +1,30 @@
+package com.codecool.myrestaurantapp.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "\"user\"")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"))
+    private List<RoleEntity> roleEntities = new ArrayList<>();
+
+}
