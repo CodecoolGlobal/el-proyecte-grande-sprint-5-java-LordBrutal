719200e52d74ca831612302a0e197ebeeedab04a
zsomborszego <szego.zsombor@gmail.com>
2022-02-23 16:03:34 +0100

    feat: user can list active orders

--- src/frontend/myrestaurantapp/src/pages/ActiveOrders.jsx
+++ src/frontend/myrestaurantapp/src/pages/ActiveOrders.jsx
@@ -2,17 +2,22 @@ import {useEffect, useState} from "react";
 import OrderFoodsList from "../components/OrderFoodsList";
 import Button from "../components/Button";
 import Collapsible from "../components/Collapsible";
+import AuthHeader from "../services/AuthHeader";
+import axios from "axios";
 
 
 function ActiveOrders(props) {
 
     const [jsonResult, setJson] = useState({isLoaded: false});
     useEffect(() => {
-        fetch("http://localhost:8080/api/get-orders")
-            .then(res => res.json())
+        const config = {
+            headers: AuthHeader()
+        }
+        const url = "http://localhost:8080/api/get-orders"
+        axios.get(url, config)
             .then(
                 (result) => {
-                    setJson({data: result, isLoaded: true})
+                    setJson({data: result.data, isLoaded: true})
                 },
                 (error) => {
                     setJson({
@@ -28,38 +33,40 @@ function ActiveOrders(props) {
             <div>
                 <h1 className={"page-title"}>Active orders</h1>
                 {jsonResult.data.map((order, index) => (
-                    <Collapsible content={order.customer.name} time={order.formattedDateTime}>
+                    <Collapsible key={index} content={order.customer.name} time={order.formattedDateTime}>
                         <div className="order-container">
-                                    <table>
-                                        <tr>
-                                            <td>Order Number</td>
-                                            <td>{order.id}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Order time</td>
-                                            <td>{order.formattedDateTime}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Customer Name</td>
-                                            <td>{order.customer.name}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Customer Adress</td>
-                                            <td>{order.customer.address.cityName}, {order.customer.address.streetName} {order.customer.address.houseNumber}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Phone Number</td>
-                                            <td>{order.customer.phoneNumber}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Total Price</td>
-                                            <td>{order.totalPrice} HUF</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Ordered Foods</td>
-                                            <td><OrderFoodsList data={order.foods}/></td>
-                                        </tr>
-                                    </table>
+                            <table>
+                                <tbody>
+                                <tr>
+                                    <td>Order Number</td>
+                                    <td>{order.id}</td>
+                                </tr>
+                                <tr>
+                                    <td>Order time</td>
+                                    <td>{order.formattedDateTime}</td>
+                                </tr>
+                                <tr>
+                                    <td>Customer Name</td>
+                                    <td>{order.customer.name}</td>
+                                </tr>
+                                <tr>
+                                    <td>Customer Adress</td>
+                                    <td>{order.customer.address.cityName}, {order.customer.address.streetName} {order.customer.address.houseNumber}</td>
+                                </tr>
+                                <tr>
+                                    <td>Phone Number</td>
+                                    <td>{order.customer.phoneNumber}</td>
+                                </tr>
+                                <tr>
+                                    <td>Total Price</td>
+                                    <td>{order.totalPrice} HUF</td>
+                                </tr>
+                                <tr>
+                                    <td>Ordered Foods</td>
+                                    <td><OrderFoodsList data={order.foods}/></td>
+                                </tr>
+                                </tbody>
+                            </table>
                             <Button className={"order-button add-button"} text={"Delete order"}
                                     url={`http://localhost:8080/api/delete-order/${order.id}`}/>
                             <Button className={"order-button add-button"} text={"Fulfilled"}
