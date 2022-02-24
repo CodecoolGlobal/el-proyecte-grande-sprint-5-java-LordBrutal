5541eda76d00b6e88d1c9e020bb470a5eaaac3ea
LordBrutal <bogar.jozsef@gmail.com>
2022-02-24 12:03:21 +0100

    add: first runner database initializer

--- src/main/java/com/codecool/myrestaurantapp/MyrestaurantappApplication.java
+++ src/main/java/com/codecool/myrestaurantapp/MyrestaurantappApplication.java
@@ -1,5 +1,9 @@
 package com.codecool.myrestaurantapp;
 
+import com.codecool.myrestaurantapp.model.entity.RoleEntity;
+import com.codecool.myrestaurantapp.model.entity.UserEntity;
+import com.codecool.myrestaurantapp.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class MyrestaurantappApplication {
@@ -21,4 +26,17 @@ public class MyrestaurantappApplication {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder(ENCODER_STRENGTH, new SecureRandom());
 	}
+
+	/** Initialize base roles, and default user */
+/*
+	@Bean
+	CommandLineRunner runner(UserService userService) {
+		return args -> {
+			userService.saveRole(RoleEntity.builder().name("ROLE_USER").build());
+			userService.saveRole(RoleEntity.builder().name("ROLE_MANAGER").build());
+			userService.saveRole(RoleEntity.builder().name("ROLE_ADMIN").build());
+
+			userService.saveUser(UserEntity.builder().name("user").password("1234").roleEntities(new ArrayList<>()).build());
+		};
+	}*/
 }
