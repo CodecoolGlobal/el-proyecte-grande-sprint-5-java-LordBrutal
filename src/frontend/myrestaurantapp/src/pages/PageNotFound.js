20f4cf3be7884a7d8b30412266ba505144740756
zsomborszego <szego.zsombor@gmail.com>
2022-02-24 11:27:22 +0100

    add: error message if user logged in change

--- src/frontend/myrestaurantapp/src/pages/PageNotFound.js
+++ src/frontend/myrestaurantapp/src/pages/PageNotFound.js
@@ -1,10 +1,14 @@
 import error from "../images/error.svg";
+import {validateRoleUser} from "../services/UserRolesValidation";
 
 
 function PageNotFound() {
     return (<div className="center-img">
         <img className="error-logo animation-left" src={error} alt=""/>
-        <h1 className="error-text animation-top">This page is not available, login <a href="/login">here</a> !</h1>
+        {!validateRoleUser() ?
+            <h1 className="error-text animation-top">This page is not available, login <a href="/login">here</a> !
+            </h1> :
+            <h1 className="error-text animation-top">This page is not available for you <a href="/">go back</a></h1>}
     </div>)
 }
 
