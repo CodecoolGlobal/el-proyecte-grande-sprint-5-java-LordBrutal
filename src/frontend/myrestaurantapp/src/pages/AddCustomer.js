ee0b69937f4881beea99986d99dc7aceb1fa1012
zsomborszego <szego.zsombor@gmail.com>
2022-03-09 17:33:05 +0100

    refactor: AddCustomer send json data with customer details

--- src/frontend/myrestaurantapp/src/pages/AddCustomer.js
+++ src/frontend/myrestaurantapp/src/pages/AddCustomer.js
@@ -1,27 +1,84 @@
+import React, {useState} from "react";
+import {saveUser} from "../services/CustomerService";
+import {useNavigate} from "react-router-dom";
+
 function AddCustomer() {
+
+    let history = useNavigate();
+
+    const initialFormData = Object.freeze({
+        name: "",
+        email: "",
+        phoneNumber: "",
+    });
+
+    const initialFormDataAdress = Object.freeze({
+        cityName: "",
+        streetName: "",
+        houseNumber: ""
+    });
+
+    const [formData, updateFormData] = React.useState(initialFormData);
+    const [formDataAdress, updateFormDataAdrees] = React.useState(initialFormDataAdress);
+
+
+    const handleChange = (e) => {
+        if (e.target.name === "cityName" || e.target.name === "streetName" || e.target.name === "houseNumber") {
+            updateFormDataAdrees({
+                ...formDataAdress,
+                [e.target.name]: e.target.value.trim()
+            });
+        } else {
+            updateFormData({
+                ...formData,
+                [e.target.name]: e.target.value.trim()
+            });
+        }
+    };
+
+
+    const handleSubmit = () => {
+        let userJsonObject = {
+            name: formData.name,
+            email: formData.email,
+            phoneNumber: formData.phoneNumber,
+            address: {
+                cityName: formDataAdress.cityName,
+                streetName: formDataAdress.streetName,
+                houseNumber: formDataAdress.houseNumber
+            }
+        }
+        saveUser(userJsonObject).then(r => history('/'));
+    }
+
+
     return (<div className="form-container animation-show">
         <div className="add-user-backround">
-            <form action="http://localhost:8080/api/add-customer" method="POST">
-                <label className="label-text">Name<br/>
-                    <input className="label-input" type="text"  width="100px" name="name" autoComplete="off" required/>
-                </label><br/>
-                <label className="label-text">Email<br/>
-                    <input className="label-input" type="text" name="email" autoComplete="off" required/>
-                </label><br/>
-                <label className="label-text">Phone Number<br/>
-                    <input className="label-input" type="text" name="phoneNumber" autoComplete="off" required/>
-                </label><br/>
-                <label className="label-text">City Name<br/>
-                    <input className="label-input" type="text" name="cityName" autoComplete="off" required/>
-                </label><br/>
-                <label className="label-text">Street Name<br/>
-                    <input className="label-input" type="text" name="streetName" autoComplete="off" required/>
-                </label><br/>
-                <label className="label-text">House Number<br/>
-                    <input className="label-input" type="number" name="houseNumber" autoComplete="off" required/>
-                </label><br/>
-                <button className="submit-button" type="submit">Save me</button>
-            </form>
+            <label className="label-text">Name<br/>
+                <input className="label-input" type="text" width="100px" name="name" autoComplete="off"
+                       onChange={handleChange} required/>
+            </label><br/>
+            <label className="label-text">Email<br/>
+                <input className="label-input" type="text" name="email" autoComplete="off" onChange={handleChange}
+                       required/>
+            </label><br/>
+            <label className="label-text">Phone Number<br/>
+                <input className="label-input" type="text" name="phoneNumber" autoComplete="off" onChange={handleChange}
+                       required/>
+            </label><br/>
+            <label className="label-text">City Name<br/>
+                <input className="label-input" type="text" name="cityName" autoComplete="off" onChange={handleChange}
+                       required/>
+            </label><br/>
+            <label className="label-text">Street Name<br/>
+                <input className="label-input" type="text" name="streetName" autoComplete="off" onChange={handleChange}
+                       required/>
+            </label><br/>
+            <label className="label-text">House Number<br/>
+                <input className="label-input" type="number" name="houseNumber" autoComplete="off"
+                       onChange={handleChange} required/>
+            </label><br/>
+            <button className="submit-button" onClick={handleSubmit}>Save me</button>
         </div>
     </div>)
 }
